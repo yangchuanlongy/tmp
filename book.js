f69970ce5448d74026800150be96dd1069e8d13e
ysiqi <67779920+ysiqi@users.noreply.github.com>
2021-04-16 01:09:31 +0800

    Create book.js

---
+++ book.js
@@ -0,0 +1,40 @@
+{
+	"links" : {
+		"sidebar" : {
+			"Home" : "https://www.baidu.com"
+		}
+	},
+	"plugins": [
+		"-lunr",
+		"-search", 
+		"search-pro",
+		"back-to-top-button",
+		"page-toc-button",
+		"anchor-navigation-ex",
+		"expandable-chapters",
+		"chapter-fold",
+		"code",
+		"copy-code-button",
+		"splitter",
+		"pageview-count",
+		"insert-logo",
+		"custom-favicon",
+		"tbfed-pagefooter",
+		"hide-element"
+	],
+	"pluginsConfig": {
+		"insert-logo": {
+			"url": "images/logo.png",
+			"style": "background: none; max-height: 35px; min-height: 35px"
+		},
+		"favicon": "images/spacex.ico",
+		"tbfed-pagefooter": {
+			"copyright":"Copyright &copy dsx2016.com 2019",
+			"modify_label": "该文章修订时间：",
+			"modify_format": "YYYY-MM-DD HH:mm:ss"
+		},
+		"hide-element": {
+			"elements": [".gitbook-link"]
+		}
+	}
+}
